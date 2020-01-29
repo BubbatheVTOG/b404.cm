d5c2663ac6120e1af5d27b27628fdc59ac52395d
Bubba Lichvar <bubbathevtog@gmail.com>
2020-01-29 16:09:07 -0500

    starting work

--- roles/common/molecule/default/tests/test_default.py
+++ roles/common/molecule/default/tests/test_default.py
@@ -12,34 +12,8 @@ def test_firewalld_installed(host):
     assert firewalld.is_installed
 
 
-def test_cockpit_installed(host):
-    cockpit = host.package("cockpit")
-    assert cockpit.is_installed
-
-
 # TODO: Figure out why this fails on Centos7 and Fedora30
 # def test_firewalld_running_and_enabled(host):
 #     firewalld = host.service("firewalld")
 #     assert firewalld.is_running
 #     assert firewalld.is_enabled
-
-
-def test_cockpit_enabled(host):
-    cockpit = host.service("cockpit")
-    assert cockpit.is_enabled
-
-
-def test_user_bubba_created(host):
-    user = host.user("bubba")
-    assert user.exists
-    assert user.shell == "/bin/zsh"
-    assert user.home == "/home/bubba"
-    assert user.groups == ["bubba", "wheel", "b404"]
-
-
-def test_user_blink404_created(host):
-    user = host.user("blink-404")
-    assert user.exists
-    assert user.shell == "/bin/bash"
-    assert user.home == "/home/blink-404"
-    assert user.groups == ["blink-404", "b404"]
