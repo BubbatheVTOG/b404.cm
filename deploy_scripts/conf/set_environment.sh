62625a187904cedcb4b422a605e122bdd0a83471
Bubba Lichvar <bubbathevtog@gmail.com>
2020-03-14 16:01:07 -0400

    starting script

---
+++ deploy_scripts/conf/set_environment.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# The current absolute directory of the script.
+export MY_DIR="$( cd -P "$(dirname "$0")" && pwd)"
+export PROJ_ROOT=${MY_DIR}../
+
+# Setting this to true will format output.
+export FORMAT=true
+
+# Setting this to true will run verification tests.
+export VERIFY_ALL=false
+
+# Find our term width. Used for centerting text.
+export TERMINAL_WIDTH=$(tput cols)
+
+# Roles Directory
+export roles_dir=${MY_DIR}/stages/
+
+# Modules Directory
+export mods_dir=${MY_DIR}/modules/
---
+++ deploy_scripts/libs/pretty_print_funcs.sh
@@ -0,0 +1,89 @@
+#!/bin/bash
+
+# Print help arguments.
+# Usage:
+#	print_help
+function print_help () {
+	echo -e "Arguments:"
+	echo -e "\t-y" "\t\tSkip confirmation messages."
+	echo -e "\t--no-confirm"
+	echo -e "\t-F" "\t\tDisable centering of output."
+	echo -e "\t--no-format"
+	echo -e "\t-t" "\t\tRun task tests."
+	echo -e "\t--run-tests"
+}
+
+# Prints char array arugment as centered text.
+# Usage:
+# 	print_center
+function print_center () {
+	if $FORMAT; then
+		# Get first arg and the length of the arg.
+		local raw_string=$*
+		local str_len=${#raw_string}
+
+		# Print the arg centered in term.
+		local loc_pad=""
+		for (( i = 1; i <= $((($TERMINAL_WIDTH-$str_len-4)/2)); i++)); do
+			loc_pad+=" "
+		done
+		echo -e "$loc_pad$raw_string"
+	else
+		echo -e "$*"
+	fi
+}
+
+# Print our welcome banner.
+# Usage:
+#	print_banner
+function print_banner () {
+
+	if $FORMAT; then
+		local msg_pad=""
+
+		for (( i = 1; i <= $((($TERMINAL_WIDTH-51)/2)); i++)); do
+			msg_pad+=" "
+		done
+
+		echo -e "$msg_pad*************************************************"
+		echo -e "$msg_pad**                                             **"
+		echo -e "$msg_pad**         \e[33m🏆\e[0m   \e[4m\e[43mDEPLOYMENT CREATOR\e[0m\e[49m  \e[33m🏆\e[0m           **"
+		echo -e "$msg_pad**                                             **"
+		echo -e "$msg_pad*************************************************"
+	else
+		echo -e "*************************************************"
+		echo -e "**                                             **"
+		echo -e "**         \e[33m🏆\e[0m   \e[4m\e[43mDEPLOYMENT CREATOR\e[0m\e[49m  \e[33m🏆\e[0m           **"
+		echo -e "**                                             **"
+		echo -e "*************************************************"
+	fi
+}
+
+# Print a confirmation screen.
+# Usage:
+#	print_confirm
+function print_confirm () {
+
+	if $FORMAT; then
+		local msg_pad=""
+
+		for (( i = 1; i <= $((($TERMINAL_WIDTH-26)/2)); i++)); do
+			msg_pad+=" "
+		done
+
+		echo -e "$msg_pad*********************"
+		echo -e "$msg_pad**                 **"
+		echo -e "$msg_pad**   \e[41;5mCONTINUE???\e[0m   **"
+		echo -e "$msg_pad**      (Y/n)      **"
+		echo -e "$msg_pad**                 **"
+		echo -e "$msg_pad*********************"
+	else
+		echo -e "*********************"
+		echo -e "**                 **"
+		echo -e "**   \e[41;5mCONTINUE???\e[0m   **"
+		echo -e "**      (Y/n)      **"
+		echo -e "**                 **"
+		echo -e "*********************"
+
+	fi
+}
