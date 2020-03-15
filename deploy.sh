887612878adaff8ecafc69d1fa22ce60f8430ab8
Bubba Lichvar <bubbathevtog@gmail.com>
2020-03-14 22:47:11 -0400

    adding notes

--- deploy.sh
+++ deploy.sh
@@ -78,14 +78,30 @@ echo -e ""
         1.1.1 set and export IP.
     1.2 Ask user for remote user of server.
         1.2.1 set and export user of server.
-    1.3 Ask user for which branch to ship (Master,Testing, Dev).
-        1.3.1 set and export branch value.
-    1.4 Ask user if deploy adminer and logger.
-        1.4.1 set and export adminer and logger values.
-    1.5 Ask user if deployment is production or development.
-        1.5.1 set and export prod value.
-
-
+    1.3 Ask user for remote user password.
+        1.3.1 set and export remote user passsword
+    1.4 Ask user for which branch to ship (Master,Testing, Dev).
+        1.4.1 set and export branch value.
+    1.5 Ask user if deploy adminer and logger.
+        1.5.1 set and export adminer and logger values.
+    1.6 Ask user if deployment is production or development.
+        1.6.1 set and export prod value.
+    1.7 Ask user for FQDN of server.
+        1.7.1 set and exprot FQDN value.
+    1.8 Ask user if remote user name of user to create
+        1.8.1 User replies to create user
+            1.8.1.1 set and export user name
+        1.8.2 user replies to use defualt.
+            1.8.2.1 set and export default user name.
+    1.9 Ask user if remote user password or default.
+        1.9.1 User decides to provide password.
+            1.9.1 set and export default password.
+        1.9.2 User provides password.
+            1.9.2.1 hash the input
+            1.9.2.1 set and export user password.
+    2.0 Generate inventory.ini
+2. Run 
+3. Generate config documentation.
 
 
 
