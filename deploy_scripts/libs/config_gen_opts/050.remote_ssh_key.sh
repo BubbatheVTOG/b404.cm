f47b1c8792b77d8070870921d9341df32e6f3afb
Bubba Lichvar <bubbathevtog@gmail.com>
2020-03-27 16:14:19 -0400

    added some additional information to the remote ssh key step.

--- deploy_scripts/libs/config_gen_opts/050.remote_ssh_key.sh
+++ deploy_scripts/libs/config_gen_opts/050.remote_ssh_key.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
-read -e -p "Supply the location for the remote ssh key(blank for current): "
+echo -e "This script will install its own ssh keys"
+echo -e "If we need ssh keys to acess the server, supply them here."
+read -e -p "Supply the location for the remote ssh key(blank for current or none): "
 [ ! -z $REPLY ] && export REMOTE_SSH_KEY=$REPLY
